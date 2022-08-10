27f13394983f1e737eb8c2f7ead823c7803dbcc7
Cristian-pacheco <110956180+Cristian-pacheco@users.noreply.github.com>
2022-08-09 22:53:35 -0500

    Create Hoja de vida.cpp

---
+++ Hoja de vida.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <conio.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include <math.h>
+
+using namespace std;
+
+int main(){
+	
+	cout<<"cristian pacheco";
+
+getch();
+    
+    return 0;
+}
